ba2dcfc7ab69d525cb186ff88e57e9bf80935646
Khoi Do <mazino2d@gmail.com>
2026-04-18 21:01:27 +0700

    Refactor GitHub repository and branch default imports for improved readability

--- terraform/github/imports.tf
+++ terraform/github/imports.tf
@@ -1,41 +1,149 @@
 # Repositories
-import { to = module.everything_as_code.github_repository.this;  id = "everything-as-code" }
-import { to = module.mazino2d.github_repository.this;             id = "mazino2d" }
-import { to = module.mazino2d_github_io.github_repository.this;   id = "mazino2d.github.io" }
-import { to = module.caketool.github_repository.this;             id = "caketool" }
-import { to = module.jaffle_shop.github_repository.this;          id = "jaffle-shop" }
-import { to = module.sim_split.github_repository.this;            id = "sim-split" }
-import { to = module.staged_recipes.github_repository.this;       id = "staged-recipes" }
-import { to = module.a3spy.github_repository.this;                id = "a3spy" }
-import { to = module.ex_bk.github_repository.this;               id = "ex-bk" }
-import { to = module.bypass_bkel.github_repository.this;          id = "bypass-bkel" }
-import { to = module.data_crawler.github_repository.this;         id = "data-crawler" }
-import { to = module.socket_chat.github_repository.this;          id = "socket-chat" }
-import { to = module.pp_course.github_repository.this;            id = "pp-course" }
-import { to = module.ai_course.github_repository.this;            id = "ai-course" }
-import { to = module.ppl_course.github_repository.this;           id = "ppl-course" }
-import { to = module.dsa_course.github_repository.this;           id = "dsa-course" }
-import { to = module.unrolled_linked_list.github_repository.this; id = "unrolled-linked-list" }
-import { to = module.baseml.github_repository.this;               id = "baseml" }
-import { to = module.utxo_selection.github_repository.this;       id = "utxo-selection" }
-import { to = module.syscall_proc_info.github_repository.this;    id = "syscall-proc-info" }
-import { to = module.os_course.github_repository.this;            id = "os-course" }
-import { to = module.os_simulation.github_repository.this;        id = "os-simulation" }
+import {
+  to = module.everything_as_code.github_repository.this
+  id = "everything-as-code"
+}
+import {
+  to = module.mazino2d.github_repository.this
+  id = "mazino2d"
+}
+import {
+  to = module.mazino2d_github_io.github_repository.this
+  id = "mazino2d.github.io"
+}
+import {
+  to = module.caketool.github_repository.this
+  id = "caketool"
+}
+import {
+  to = module.jaffle_shop.github_repository.this
+  id = "jaffle-shop"
+}
+import {
+  to = module.sim_split.github_repository.this
+  id = "sim-split"
+}
+import {
+  to = module.staged_recipes.github_repository.this
+  id = "staged-recipes"
+}
+import {
+  to = module.a3spy.github_repository.this
+  id = "a3spy"
+}
+import {
+  to = module.ex_bk.github_repository.this
+  id = "ex-bk"
+}
+import {
+  to = module.bypass_bkel.github_repository.this
+  id = "bypass-bkel"
+}
+import {
+  to = module.data_crawler.github_repository.this
+  id = "data-crawler"
+}
+import {
+  to = module.socket_chat.github_repository.this
+  id = "socket-chat"
+}
+import {
+  to = module.pp_course.github_repository.this
+  id = "pp-course"
+}
+import {
+  to = module.ai_course.github_repository.this
+  id = "ai-course"
+}
+import {
+  to = module.ppl_course.github_repository.this
+  id = "ppl-course"
+}
+import {
+  to = module.dsa_course.github_repository.this
+  id = "dsa-course"
+}
+import {
+  to = module.unrolled_linked_list.github_repository.this
+  id = "unrolled-linked-list"
+}
+import {
+  to = module.baseml.github_repository.this
+  id = "baseml"
+}
+import {
+  to = module.utxo_selection.github_repository.this
+  id = "utxo-selection"
+}
+import {
+  to = module.syscall_proc_info.github_repository.this
+  id = "syscall-proc-info"
+}
+import {
+  to = module.os_course.github_repository.this
+  id = "os-course"
+}
+import {
+  to = module.os_simulation.github_repository.this
+  id = "os-simulation"
+}
 
 # Branch defaults
-import { to = module.everything_as_code.github_branch_default.this[0]; id = "everything-as-code" }
-import { to = module.mazino2d.github_branch_default.this[0];           id = "mazino2d" }
-import { to = module.mazino2d_github_io.github_branch_default.this[0]; id = "mazino2d.github.io" }
-import { to = module.caketool.github_branch_default.this[0];           id = "caketool" }
-import { to = module.jaffle_shop.github_branch_default.this[0];        id = "jaffle-shop" }
-import { to = module.sim_split.github_branch_default.this[0];          id = "sim-split" }
-import { to = module.staged_recipes.github_branch_default.this[0];     id = "staged-recipes" }
+import {
+  to = module.everything_as_code.github_branch_default.this[0]
+  id = "everything-as-code"
+}
+import {
+  to = module.mazino2d.github_branch_default.this[0]
+  id = "mazino2d"
+}
+import {
+  to = module.mazino2d_github_io.github_branch_default.this[0]
+  id = "mazino2d.github.io"
+}
+import {
+  to = module.caketool.github_branch_default.this[0]
+  id = "caketool"
+}
+import {
+  to = module.jaffle_shop.github_branch_default.this[0]
+  id = "jaffle-shop"
+}
+import {
+  to = module.sim_split.github_branch_default.this[0]
+  id = "sim-split"
+}
+import {
+  to = module.staged_recipes.github_branch_default.this[0]
+  id = "staged-recipes"
+}
 
 # Branch protections
-import { to = module.everything_as_code.github_branch_protection.this[0]; id = "everything-as-code:main" }
-import { to = module.mazino2d.github_branch_protection.this[0];           id = "mazino2d:main" }
-import { to = module.mazino2d_github_io.github_branch_protection.this[0]; id = "mazino2d.github.io:main" }
-import { to = module.caketool.github_branch_protection.this[0];           id = "caketool:main" }
-import { to = module.jaffle_shop.github_branch_protection.this[0];        id = "jaffle-shop:main" }
-import { to = module.sim_split.github_branch_protection.this[0];          id = "sim-split:main" }
-import { to = module.staged_recipes.github_branch_protection.this[0];     id = "staged-recipes:main" }
+import {
+  to = module.everything_as_code.github_branch_protection.this[0]
+  id = "everything-as-code:main"
+}
+import {
+  to = module.mazino2d.github_branch_protection.this[0]
+  id = "mazino2d:main"
+}
+import {
+  to = module.mazino2d_github_io.github_branch_protection.this[0]
+  id = "mazino2d.github.io:main"
+}
+import {
+  to = module.caketool.github_branch_protection.this[0]
+  id = "caketool:main"
+}
+import {
+  to = module.jaffle_shop.github_branch_protection.this[0]
+  id = "jaffle-shop:main"
+}
+import {
+  to = module.sim_split.github_branch_protection.this[0]
+  id = "sim-split:main"
+}
+import {
+  to = module.staged_recipes.github_branch_protection.this[0]
+  id = "staged-recipes:main"
+}
