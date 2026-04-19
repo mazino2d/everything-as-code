6aeaa9e339013e4893bd5a04c47bed93a342950d
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-04-19 10:08:25 +0700

    fix: correct module source path for GCP project

--- terraform/gcp/mazino2d-as-se1-dev/main.tf
+++ terraform/gcp/mazino2d-as-se1-dev/main.tf
@@ -1,5 +1,5 @@
 module "project" {
-  source             = "../_modules/gcp-project"
+  source             = "./_modules/gcp-project"
   name               = "Mazino2D's Dev Env"
   project_id         = "mazino2d-as-se1-dev"
   billing_account_id = "016BA1-3DBFBB-1B7972"
