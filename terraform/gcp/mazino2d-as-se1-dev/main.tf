5c18afeb3bb2005bd6c4b03afc22d5cf32a53f5b
Khoi Do <mazino2d@gmail.com>
2026-04-20 20:57:13 +0700

    config: enable k3s Traefik (#10)

--- terraform/gcp/mazino2d-as-se1-dev/main.tf
+++ terraform/gcp/mazino2d-as-se1-dev/main.tf
@@ -17,6 +17,6 @@ module "vm" {
   zone           = "us-central1-a"
   machine_type   = "e2-micro"
   ssh_public_key = var.ssh_public_key
-  extra_ports    = ["6443"]
+  extra_ports    = ["6443", "80", "443"]
   startup_script = file("${path.module}/_scripts/install_k3s.sh")
 }
