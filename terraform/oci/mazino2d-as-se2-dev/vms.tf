7d60e13ad3de2b89671d60f9b0b92a05a4e38445
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-04-24 16:58:08 +0700

    refactor: rename VM module from vm_micro to vm for clarity

--- terraform/oci/mazino2d-as-se2-dev/vms.tf
+++ terraform/oci/mazino2d-as-se2-dev/vms.tf
@@ -1,6 +1,6 @@
-module "vm_micro" {
+module "vm" {
   source         = "./_modules/oci-vm"
-  name           = "vm-micro"
+  name           = "vm"
   compartment_id = module.compartment.compartment_id
   shape          = "VM.Standard.E2.1.Micro"
   ssh_public_key = var.ssh_public_key
