135b3562a1e757edb3f8e3a97500eaff0bde98b6
Khoi Do <mazino2d@gmail.com>
2026-04-24 16:58:41 +0700

    fix: Add external IP retrieval for K3s installation script (#17)

--- terraform/gcp/mazino2d-as-se1-dev/_scripts/install_k3s.sh
+++ terraform/gcp/mazino2d-as-se1-dev/_scripts/install_k3s.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable servicelb --disable metrics-server --write-kubeconfig-mode 644" sh -
+EXTERNAL_IP=$(curl -sf -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/externalIp)
+
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable servicelb --disable metrics-server --write-kubeconfig-mode 644 --tls-san ${EXTERNAL_IP}" sh -
