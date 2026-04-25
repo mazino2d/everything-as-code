491d1fd688acf810d20ddb1ae1b286808b8e4b3b
Khoi Do <mazino2d@gmail.com>
2026-04-25 11:41:58 +0700

    feat: update gcp vm module & config new e2-small vm (#22)
    Co-authored-by: Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>

--- terraform/gcp/mazino2d-as-se1-dev/vms.tf
+++ terraform/gcp/mazino2d-as-se1-dev/vms.tf
@@ -1,10 +1,13 @@
 module "vm" {
-  source         = "./_modules/gcp-vm"
-  name           = "vm"
-  project_id     = module.project.project_id
-  zone           = "us-central1-a"
-  machine_type   = "e2-micro"
-  ssh_public_key = var.ssh_public_key
-  extra_ports    = ["6443", "80", "443"]
-  startup_script = file("${path.module}/_scripts/install_k3s.sh")
+  source           = "./_modules/gcp-vm"
+  name             = "vm"
+  project_id       = module.project.project_id
+  zone             = "asia-southeast1-b"
+  machine_type     = "e2-small"
+  spot             = true
+  enable_static_ip = true
+  enable_internet  = true
+  extra_ports       = ["6443", "80", "443"]
+  ssh_public_key   = var.ssh_public_key
+  startup_script    = file("${path.module}/_scripts/install_k3s.sh")
 }
