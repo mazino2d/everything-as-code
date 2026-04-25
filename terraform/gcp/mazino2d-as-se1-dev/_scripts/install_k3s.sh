ed4846ffc094ee4256597aba95123381cc6c8023
Khoi Do <mazino2d@gmail.com>
2026-04-26 04:07:03 +0700

    chore: remove tf-apply scheduler (#32)
    Co-authored-by: Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>

--- terraform/gcp/mazino2d-as-se1-dev/_scripts/install_k3s.sh
+++ terraform/gcp/mazino2d-as-se1-dev/_scripts/install_k3s.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+METADATA_URL="http://metadata.google.internal/computeMetadata/v1/instance"
+
 EXTERNAL_IP=$(curl -sf -H "Metadata-Flavor: Google" \
-  "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/?recursive=true" \
+  "${METADATA_URL}/network-interfaces/0/?recursive=true" \
   | grep -oP '"externalIp":"\K[^"]+' | head -1)
 
 TLS_SAN_ARGS=""
 [ -n "$EXTERNAL_IP" ] && TLS_SAN_ARGS="${TLS_SAN_ARGS} --tls-san ${EXTERNAL_IP}"
 
-DUCKDNS_TOKEN=$(curl -sf -H "Metadata-Flavor: Google" \
-  "http://metadata.google.internal/computeMetadata/v1/instance/attributes/duckdns-token")
-DUCKDNS_DOMAIN=$(curl -sf -H "Metadata-Flavor: Google" \
-  "http://metadata.google.internal/computeMetadata/v1/instance/attributes/duckdns-domain")
+DUCKDNS_TOKEN=$(curl -sf -H "Metadata-Flavor: Google" "${METADATA_URL}/attributes/duckdns-token")
+DUCKDNS_DOMAIN=$(curl -sf -H "Metadata-Flavor: Google" "${METADATA_URL}/attributes/duckdns-domain")
 
 if [ -n "$DUCKDNS_TOKEN" ] && [ -n "$DUCKDNS_DOMAIN" ]; then
   result=$(curl -sf "https://www.duckdns.org/update?domains=${DUCKDNS_DOMAIN}&token=${DUCKDNS_TOKEN}&ip=${EXTERNAL_IP}&verbose=true")
