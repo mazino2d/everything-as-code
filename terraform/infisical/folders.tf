8376e3bdc0d3038ced53ee98f56cb8bd1904eb9e
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-04-27 13:18:15 +0700

    fix: update variable names for consistency in Infisical folder resources

--- terraform/infisical/folders.tf
+++ terraform/infisical/folders.tf
@@ -4,20 +4,20 @@ locals {
 
 # monitoring/
 resource "infisical_secret_folder" "monitoring" {
-  for_each     = toset(local.environments)
-  name         = "monitoring"
-  env_slug     = each.value
-  workspace_id = module.everything_as_code.id
-  folder_path  = "/"
+  for_each         = toset(local.environments)
+  name             = "monitoring"
+  environment_slug = each.value
+  project_id       = module.everything_as_code.id
+  folder_path      = "/"
 }
 
 # monitoring/grafana-alloy
 resource "infisical_secret_folder" "monitoring_grafana_alloy" {
-  for_each     = toset(local.environments)
-  name         = "grafana-alloy"
-  env_slug     = each.value
-  workspace_id = module.everything_as_code.id
-  folder_path  = "/monitoring"
+  for_each         = toset(local.environments)
+  name             = "grafana-alloy"
+  environment_slug = each.value
+  project_id       = module.everything_as_code.id
+  folder_path      = "/monitoring"
 
   depends_on = [infisical_secret_folder.monitoring]
 }
