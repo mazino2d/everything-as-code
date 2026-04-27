a9741a62ca99509094bf89c805ec8e89c2bb514e
Khoi Do <mazino2d@gmail.com>
2026-04-28 06:36:18 +0700

    feat: intergrate loki (#54)

--- terraform/grafana/_outputs.tf
+++ terraform/grafana/_outputs.tf
@@ -10,3 +10,16 @@ output "prometheus_password" {
   value     = module.grafana_cloud_dev.prometheus_password
   sensitive = true
 }
+
+output "loki_endpoint" {
+  value = module.grafana_cloud_dev.loki_endpoint
+}
+
+output "loki_username" {
+  value = module.grafana_cloud_dev.loki_username
+}
+
+output "loki_password" {
+  value     = module.grafana_cloud_dev.loki_password
+  sensitive = true
+}
