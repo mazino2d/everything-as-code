b5add173444a4f3e692b85c5e7ed5e149cb0c8a7
Khoi Do <mazino2d@gmail.com>
2026-04-27 12:34:29 +0700

    feat: add Grafana integration with Terraform configuration and setup documentation (#40)
    Co-authored-by: Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>

---
+++ terraform/grafana/dev_stack.tf
@@ -0,0 +1,31 @@
+module "grafana_cloud_dev" {
+  source = "./_modules/grafana-cloud-stack"
+
+  stack_slug        = "mazino2ddev"
+  stack_name        = "mazino2ddev"
+  stack_region_slug = "ap-southeast-0"
+}
+
+resource "infisical_secret" "dev_remote_write_url" {
+  name         = "REMOTE_WRITE_URL"
+  value        = module.grafana_cloud_dev.prometheus_remote_write_url
+  env_slug     = "dev"
+  workspace_id = var.infisical_project_id
+  folder_path  = "/grafana-alloy"
+}
+
+resource "infisical_secret" "dev_remote_write_username" {
+  name         = "REMOTE_WRITE_USERNAME"
+  value        = module.grafana_cloud_dev.prometheus_username
+  env_slug     = "dev"
+  workspace_id = var.infisical_project_id
+  folder_path  = "/grafana-alloy"
+}
+
+resource "infisical_secret" "dev_remote_write_password" {
+  name         = "REMOTE_WRITE_PASSWORD"
+  value        = module.grafana_cloud_dev.prometheus_password
+  env_slug     = "dev"
+  workspace_id = var.infisical_project_id
+  folder_path  = "/grafana-alloy"
+}
