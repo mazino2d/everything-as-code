4c173535509ea0225af04406159135387fefab5b
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-04-27 15:54:17 +0700

    feat: Add client secrets management for cicd and k8s-operator identities

--- terraform/infisical/_modules/infisical-identity/_outputs.tf
+++ terraform/infisical/_modules/infisical-identity/_outputs.tf
@@ -5,3 +5,12 @@ output "id" {
 output "name" {
   value = infisical_identity.this.name
 }
+
+output "client_id" {
+  value = infisical_identity_universal_auth.this.client_id
+}
+
+output "client_secrets" {
+  value     = { for k, v in infisical_identity_universal_auth_client_secret.this : k => v.client_secret }
+  sensitive = true
+}
