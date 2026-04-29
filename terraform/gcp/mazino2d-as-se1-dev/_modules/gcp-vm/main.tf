60c35b064ec439166c1522fbafc4ad4ab0a5fda3
Khoi Do <mazino2d@gmail.com>
2026-04-29 16:42:03 +0700

    feat: Add allow_stopping_for_update variable to VM module (#64)
    Co-authored-by: Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>

--- terraform/gcp/mazino2d-as-se1-dev/_modules/gcp-vm/main.tf
+++ terraform/gcp/mazino2d-as-se1-dev/_modules/gcp-vm/main.tf
@@ -44,11 +44,12 @@ resource "google_compute_firewall" "extra_ports" {
 }
 
 resource "google_compute_instance" "this" {
-  name         = var.name
-  machine_type = var.machine_type
-  zone         = var.zone
-  project      = var.project_id
-  tags         = concat(["${var.name}-ssh"], var.tags)
+  name                      = var.name
+  machine_type              = var.machine_type
+  zone                      = var.zone
+  project                   = var.project_id
+  tags                      = concat(["${var.name}-ssh"], var.tags)
+  allow_stopping_for_update = var.allow_stopping_for_update
 
   boot_disk {
     initialize_params {
