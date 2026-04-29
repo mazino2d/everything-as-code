f63587038765f60b13b0f597cb3289df5469a484
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-04-29 16:57:16 +0700

    feat: Refactor ArgoCD application management with local variables and inline manifests

--- terraform/k8s/argocd.tf
+++ terraform/k8s/argocd.tf
@@ -1,3 +1,11 @@
+locals {
+  argocd_apps = {
+    infra      = "kubernetes/clusters/mazino2d-as-se1-dev/infra"
+    monitoring = "kubernetes/clusters/mazino2d-as-se1-dev/monitoring"
+    apps       = "kubernetes/clusters/mazino2d-as-se1-dev/apps"
+  }
+}
+
 resource "tls_private_key" "argocd" {
   algorithm   = "ECDSA"
   ecdsa_curve = "P256"
@@ -14,7 +22,46 @@ resource "helm_release" "argocd" {
   version          = "7.7.5"
   namespace        = "argocd"
   create_namespace = true
-  values           = [file("${path.module}/argocd-values.yaml")]
+  values = [<<-YAML
+    configs:
+      cm:
+        kustomize.buildOptions: "--enable-helm"
+  YAML
+  ]
+}
+
+resource "kubernetes_manifest" "argocd_application" {
+  for_each = local.argocd_apps
+
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = each.key
+      namespace = "argocd"
+      finalizers = ["resources-finalizer.argocd.argoproj.io"]
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "git@github.com:mazino2d/everything-as-code.git"
+        targetRevision = "main"
+        path           = each.value
+      }
+      destination = {
+        server = "https://kubernetes.default.svc"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = ["CreateNamespace=true", "ServerSideApply=true"]
+      }
+    }
+  }
+
+  depends_on = [helm_release.argocd]
 }
 
 resource "kubernetes_secret" "argocd_repo_creds" {
