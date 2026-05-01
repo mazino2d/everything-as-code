55f37d089530fea224038de3ab3987293caaec99
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-05-01 22:33:03 +0700

    feat: Enhance ArgoCD application configuration with sync options and retry policy

--- terraform/k8s/argocd.tf
+++ terraform/k8s/argocd.tf
@@ -1,3 +1,40 @@
+locals {
+  argocd_apps = {
+    infra = {
+      path         = "kubernetes/clusters/mazino2d-as-se1-dev/infra"
+      sync_wave    = 0
+      sync_options = [
+        "CreateNamespace=true",
+        "ServerSideApply=true",
+        "ApplyOutOfSyncOnly=true",
+        "PruneLast=true",
+      ]
+    }
+    monitoring = {
+      path         = "kubernetes/clusters/mazino2d-as-se1-dev/monitoring"
+      sync_wave    = 1
+      sync_options = [
+        "CreateNamespace=true",
+        "ServerSideApply=true",
+        "ApplyOutOfSyncOnly=true",
+        "PruneLast=true",
+        "SkipDryRunOnMissingResource=true",
+      ]
+    }
+    apps = {
+      path         = "kubernetes/clusters/mazino2d-as-se1-dev/apps"
+      sync_wave    = 2
+      sync_options = [
+        "CreateNamespace=true",
+        "ServerSideApply=true",
+        "ApplyOutOfSyncOnly=true",
+        "PruneLast=true",
+        "SkipDryRunOnMissingResource=true",
+      ]
+    }
+  }
+}
+
 data "terraform_remote_state" "github" {
   backend = "remote"
 
@@ -6,14 +43,6 @@ data "terraform_remote_state" "github" {
     workspaces = {
       name = "github"
     }
-  }
-}
-
-locals {
-  argocd_apps = {
-    infra      = "kubernetes/clusters/mazino2d-as-se1-dev/infra"
-    monitoring = "kubernetes/clusters/mazino2d-as-se1-dev/monitoring"
-    apps       = "kubernetes/clusters/mazino2d-as-se1-dev/apps"
   }
 }
 
@@ -41,32 +70,51 @@ resource "helm_release" "argocd" {
 resource "kubectl_manifest" "argocd_application" {
   for_each = local.argocd_apps
 
-  yaml_body = <<-YAML
-    apiVersion: argoproj.io/v1alpha1
-    kind: Application
-    metadata:
-      name: ${each.key}
-      namespace: argocd
-      finalizers:
-        - resources-finalizer.argocd.argoproj.io
-    spec:
-      project: default
-      source:
-        repoURL: git@github.com:mazino2d/everything-as-code.git
-        targetRevision: main
-        path: ${each.value}
-      destination:
-        server: https://kubernetes.default.svc
-      syncPolicy:
-        automated:
-          prune: true
-          selfHeal: true
-        syncOptions:
-          - CreateNamespace=true
-          - ServerSideApply=true
-  YAML
+  yaml_body = yamlencode({
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = each.key
+      namespace = "argocd"
+      annotations = {
+        "argocd.argoproj.io/sync-wave" = tostring(each.value.sync_wave)
+      }
+      finalizers = [
+        "resources-finalizer.argocd.argoproj.io",
+      ]
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "git@github.com:mazino2d/everything-as-code.git"
+        targetRevision = "main"
+        path           = each.value.path
+      }
+      destination = {
+        server = "https://kubernetes.default.svc"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        retry = {
+          limit = 20
+          backoff = {
+            duration    = "15s"
+            factor      = 2
+            maxDuration = "10m"
+          }
+        }
+        syncOptions = each.value.sync_options
+      }
+    }
+  })
 
-  depends_on = [helm_release.argocd]
+  depends_on = [
+    helm_release.argocd,
+    kubernetes_secret.argocd_repo_creds,
+  ]
 }
 
 resource "kubernetes_secret" "argocd_repo_creds" {
