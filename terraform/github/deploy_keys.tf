35dd4f4c75509e57e90cb27b29067b100b8956b9
Khoi Do <mazino2d@gmail.com>
2026-05-07 06:20:19 +0700

    refactor: Move ArgoCD key outputs and remove Grafana vars (#129)
    Add terraform/github/_outputs.tf to expose argocd_public_key and
    sensitive argocd_private_key_pem, and remove the duplicate outputs from
    terraform/github/deploy_keys.tf. Also delete
    terraform/grafana/dashboard/_variables.tf and
    terraform/grafana/dashboard/terraform.tfvars.example (remove
    unused/stale dashboard variable files). This separates outputs for
    clarity and cleans up unused Grafana dashboard examples.

--- terraform/github/deploy_keys.tf
+++ terraform/github/deploy_keys.tf
@@ -9,12 +9,3 @@ resource "github_repository_deploy_key" "argocd" {
   key        = tls_private_key.argocd.public_key_openssh
   read_only  = true
 }
-
-output "argocd_public_key" {
-  value = tls_private_key.argocd.public_key_openssh
-}
-
-output "argocd_private_key_pem" {
-  value     = tls_private_key.argocd.private_key_pem
-  sensitive = true
-}
