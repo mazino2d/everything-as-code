3ce3f5c1fba779512df34fe84c95910b7c8aff11
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-05-09 20:43:05 +0700

    config: Update GKE cluster configuration and remove deprecated node pool

--- terraform/gcp/mazino2d-as-se1-dev/gke.tf
+++ terraform/gcp/mazino2d-as-se1-dev/gke.tf
@@ -1,10 +1,13 @@
 resource "google_container_cluster" "this" {
   name     = "mazino2d-as-se1-dev"
-  location = "asia-southeast1-b"
+  location = "asia-southeast1-c"
   project  = module.project.project_id
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
+  node_locations = [
+    "asia-southeast1-c", 
+    "asia-southeast1-b",
+    "asia-southeast1-a"
+  ]
 
   deletion_protection = false
 
@@ -19,6 +22,37 @@ resource "google_container_cluster" "this" {
 
   logging_service    = "none"
   monitoring_service = "none"
+
+  node_pool {
+    name = "default-pool"
+
+    autoscaling {
+      total_min_node_count = 0
+      total_max_node_count = 1
+      location_policy      = "ANY"
+    }
+
+    management {
+      auto_repair  = true
+      auto_upgrade = true
+    }
+
+    node_config {
+      machine_type = "t2d-standard-2"
+      spot         = true
+      disk_size_gb = 20
+      disk_type    = "pd-standard"
+
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/trace.append",
+      ]
+    }
+  }
 
   depends_on = [module.project]
 }
@@ -37,39 +71,4 @@ resource "google_project_iam_member" "k8s_tf_container_developer" {
 
 resource "google_service_account_key" "k8s_tf" {
   service_account_id = google_service_account.k8s_tf.name
-}
-
-resource "google_container_node_pool" "spot" {
-  name           = "spot-pool"
-  location       = "asia-southeast1-b"
-  cluster        = google_container_cluster.this.name
-  project        = module.project.project_id
-  node_locations = ["asia-southeast1-a", "asia-southeast1-b", "asia-southeast1-c"]
-
-  autoscaling {
-    total_min_node_count = 0
-    total_max_node_count = 1
-    location_policy      = "ANY"
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  node_config {
-    machine_type = "t2d-standard-2"
-    spot         = true
-    disk_size_gb = 20
-    disk_type    = "pd-standard"
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/trace.append",
-    ]
-  }
 }
