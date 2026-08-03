5b1dc060dfb7c3d35d8a30ad5dbb7668ae78a33d
Khôi Đỗ (Sr. Data Scientist) <khoi.do@cake.vn>
2026-08-03 20:58:05 +0700

    Update vms.tf

--- terraform/gcp/mazino2d-as-se1-dev/vms.tf
+++ terraform/gcp/mazino2d-as-se1-dev/vms.tf
@@ -8,7 +8,7 @@ module "vm-free" {
   spot             = false
   external_ip_type = "ephemeral"
   extra_ports      = ["80"]
-  ssh_public_key   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHqUrfB0oPmolyXRYtA9kHDWYy5D2GhhaGb9odfQYvAu"
+  ssh_public_key   = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFN11kywcyrDb/iiguUkDGFAgGTqvzVmsIG7Fv4+HYHLuPZGKsCrTv9L7BD5BLeZ6wlZDvWwkHEGW7+RzMC235y8gvkZIEfx1Wz3rOMhqmw4lc1miCVTX8pXL5ypyTjSWaMvcRaOczV8ewg+sgvHazEuy/i/t42NmVrZx3je9qI09FsHetF42zCbIf+2FpxG8OZfGUi6GfuKDi4pztO+wHfKpcoDZOzVKG3ZHHimXu342Zgbb2sFxx69qpxt+ntcGdFiEiE6U4nhCetL7xX7PEiyOs8Yr0XS41DTSsvR3Ejx1BWPfe/k42Bu2G2yqM2G3iPtqo4X1glDQpAQlIdRzZ ddkhoa@ddkhoa"
   tags             = ["free"]
   duckdns_domain   = "mazino2d-free"
   startup_script   = file("${path.module}/_scripts/fortune_server.sh")
